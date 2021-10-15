dfd6e84a1254d2a182476c067ac22c2946587d98
Callum Macpherson <callum.macpherson@hackney.gov.uk>
2021-10-15 16:04:01 +0100

    Add storage controller

---
+++ backend/document-service/DocumentService/DocumentService.Tests/Controllers/StorageControllerTests.cs
@@ -0,0 +1,61 @@
+﻿using AutoFixture;
+using DocumentService.Boundary.Request;
+using DocumentService.Boundary.Response;
+using DocumentService.Controllers;
+using DocumentService.Domain;
+using DocumentService.Infrastructure.Exceptions;
+using DocumentService.Tests.Helpers;
+using DocumentService.UseCase.Interfaces;
+using FluentAssertions;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+
+namespace DocumentService.Tests.Controllers
+{
+    public class StorageControllerTests
+    {
+        private readonly StorageController _storageController;
+        private readonly Mock<IGetStorageUsageUseCase> _mockGetStorageUsageUseCase;
+
+        private readonly Fixture _fixture = new Fixture();
+        private readonly Random _random = new Random();
+
+        public StorageControllerTests()
+        {
+            _mockGetStorageUsageUseCase = new Mock<IGetStorageUsageUseCase>();
+
+            _storageController = new StorageController(_mockGetStorageUsageUseCase.Object);
+
+            _storageController.ControllerContext = new ControllerContext();
+            _storageController.ControllerContext.HttpContext = new DefaultHttpContext();
+            _storageController.ControllerContext.HttpContext.Items["user"] = ContextHelper.CreateUser();
+        }
+
+        [Fact]
+        public async Task GetStorageUsage_WhenCalled_CallsUseCase()
+        {
+            // Arrange
+            var useCaseResponse = _fixture.Create<GetStorageUsageResponse>();
+
+            _mockGetStorageUsageUseCase
+                .Setup(x => x.Execute(It.IsAny<Guid>()))
+                .ReturnsAsync(useCaseResponse);
+
+            // Act
+            var response = await _storageController.GetStorageUsage();
+
+            // Assert
+            response.Should().BeOfType(typeof(OkObjectResult));
+            (response as OkObjectResult).Value.Should().BeOfType(typeof(GetStorageUsageResponse));
+            ((response as OkObjectResult).Value as GetStorageUsageResponse).Capacity.Should().Be(useCaseResponse.Capacity);
+            ((response as OkObjectResult).Value as GetStorageUsageResponse).StorageUsage.Should().Be(useCaseResponse.StorageUsage);
+        }
+    }
+}
---
+++ backend/document-service/DocumentService/DocumentService/Controllers/StorageController.cs
@@ -0,0 +1,40 @@
+﻿using DocumentService.Boundary.Response;
+using DocumentService.Logging;
+using DocumentService.UseCase.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TokenService.Models;
+
+namespace DocumentService.Controllers
+{
+    [Route("api/storage")]
+    [ApiController]
+    public class StorageController : ControllerBase
+    {
+        private readonly IGetStorageUsageUseCase _getStorageUsageUseCase;
+
+        public StorageController(IGetStorageUsageUseCase getStorageUsageUseCase)
+        {
+            _getStorageUsageUseCase = getStorageUsageUseCase;
+        }
+
+        [Route("usage")]
+        [HttpGet]
+        [ProducesResponseType(typeof(GetStorageUsageResponse), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<IActionResult> GetStorageUsage()
+        {
+            LogHelper.LogController("GetStorageUsage");
+
+            var user = (User)HttpContext.Items["user"];
+
+            var response = await _getStorageUsageUseCase.Execute(user.Id);
+
+            return Ok(response);
+        }
+    }
+}
