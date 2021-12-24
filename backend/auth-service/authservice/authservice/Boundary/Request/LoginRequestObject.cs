955b38ca7d333aecd039562e492c0f1ae86cf031
Callum Macpherson <callum.macpherson@hackney.gov.uk>
2021-12-24 11:13:22 +0000

    Delete old AuthService project

--- backend/auth-service/authservice/authservice/Boundary/Request/LoginRequestObject.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace authservice.Boundary.Request
-{
-    public class LoginRequestObject
-    {
-        public string Email { get; set; }
-        public string Password { get; set; }
-    }
-}
