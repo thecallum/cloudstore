f0086ab5424b8da930b3c13ebd3d2ac0b0be48a7
Callum Macpherson <callum.macpherson@hackney.gov.uk>
2022-02-19 16:24:45 +0000

    Update Header

--- frontend/src/views/layout/header.js
+++ frontend/src/views/layout/header.js
@@ -1,25 +1,43 @@
 import HeaderLinks from "./headerLinks";
+import styled from "styled-components";
+
+const HeaderStyled = styled.header`
+  background: hsl(180, 20%, 50%);
+`;
+
+const HeaderContainerStyled = styled.div`
+  // padding: 30px;
+
+  padding: 15px 0;
+  display: flex;
+  justify-content: space-between;
+  flex-direction: column;
+
+  width: calc(100% - 30px);
+  max-width: 800px;
+  margin: 0 auto;
+
+  @media(min-width: 400px) {
+    flex-direction: row;
+    align-items: center;
+
+  }
+
+`;
 
 const header = () => {
 
 
   return (
-    <header
-      style={{
-        background: "hsl(180, 20%, 50%)",
-        padding: "30px",
-        display: "flex",
-        justifyContent: "space-between",
-        alignItems: "center",
-      }}
-    >
-      <div style={{
-        fontSize: "26px"
-      }}>CloudStore</div>
+    <HeaderStyled>
+      <HeaderContainerStyled>
+        <div style={{
+          fontSize: "26px"
+        }}>CloudStore</div>
 
-      <HeaderLinks />
-      
-    </header>
+        <HeaderLinks />
+      </HeaderContainerStyled>
+    </HeaderStyled>
   );
 };
 
