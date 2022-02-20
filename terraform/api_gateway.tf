6542af5b8eb35212fadcdfc4fb2a9aef60a4919b
Callum Macpherson <callummac@protonmail.com>
2022-02-20 19:39:49 +0000

    Implement terraform (#20)
    * Add base terraform to create Lambda function with API Gateway
    * add terraform apply
    * fix format
    * add s3 backend to terraform
    
    Co-authored-by: Callum Macpherson <callum.macpherson@hackney.gov.uk>

---
+++ terraform/api_gateway.tf
@@ -0,0 +1,58 @@
+resource "aws_api_gateway_rest_api" "apiGateway" {
+  name        = "CloudStore-API"
+  description = "Terraform Serverless Application Example"
+}
+
+resource "aws_api_gateway_resource" "proxy" {
+  rest_api_id = "${aws_api_gateway_rest_api.apiGateway.id}"
+  parent_id   = "${aws_api_gateway_rest_api.apiGateway.root_resource_id}"
+  path_part   = "{proxy+}"
+}
+
+resource "aws_api_gateway_method" "proxy" {
+  rest_api_id   = "${aws_api_gateway_rest_api.apiGateway.id}"
+  resource_id   = "${aws_api_gateway_resource.proxy.id}"
+  http_method   = "ANY"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_integration" "lambda" {
+  rest_api_id = "${aws_api_gateway_rest_api.apiGateway.id}"
+  resource_id = "${aws_api_gateway_method.proxy.resource_id}"
+  http_method = "${aws_api_gateway_method.proxy.http_method}"
+
+  integration_http_method = "POST"
+  type                    = "AWS_PROXY"
+  uri                     = "${aws_lambda_function.DocumentService.invoke_arn}"
+}
+
+resource "aws_api_gateway_method" "proxy_root" {
+  rest_api_id   = "${aws_api_gateway_rest_api.apiGateway.id}"
+  resource_id   = "${aws_api_gateway_rest_api.apiGateway.root_resource_id}"
+  http_method   = "ANY"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_integration" "lambda_root" {
+  rest_api_id = "${aws_api_gateway_rest_api.apiGateway.id}"
+  resource_id = "${aws_api_gateway_method.proxy_root.resource_id}"
+  http_method = "${aws_api_gateway_method.proxy_root.http_method}"
+
+  integration_http_method = "POST"
+  type                    = "AWS_PROXY"
+  uri                     = "${aws_lambda_function.DocumentService.invoke_arn}"
+}
+
+resource "aws_api_gateway_deployment" "example" {
+  depends_on = [
+    aws_api_gateway_integration.lambda,
+    aws_api_gateway_integration.lambda_root,
+  ]
+
+  rest_api_id = "${aws_api_gateway_rest_api.apiGateway.id}"
+  stage_name  = "test"
+}
+
+output "base_url" {
+  value = "${aws_api_gateway_deployment.example.invoke_url}"
+}
---
+++ terraform/main.tf
@@ -0,0 +1,69 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.27"
+    }
+  }
+
+  required_version = ">= 0.14.9"
+
+  backend "s3" {
+    bucket = "terraform-state-cloudstore"
+    key    = "terraform/DocumentService/terraform.tfstate"
+    region = "eu-west-1"
+  }
+}
+
+provider "aws" {
+  region  = "eu-west-1"
+}
+
+variable "DatabaseConnectionString" {
+    type = string
+}
+
+variable "SECRET" {
+    type = string
+}
+
+
+resource "aws_lambda_function" "DocumentService" {
+  function_name = "DocumentService"
+  role = "arn:aws:iam::714664911966:role/DocumentServiceRole"
+  handler = "DocumentService::DocumentService.LambdaEntryPoint::FunctionHandlerAsync"
+  runtime = "dotnetcore3.1"
+
+  s3_bucket = "terraform-state-cloudstore"
+  s3_key = "DocumentService/DocumentService.zip"
+
+  description = "description..."
+
+  timeout = 300
+  memory_size = 256
+
+  vpc_config {
+    subnet_ids         = ["subnet-a4ce21ef", "subnet-96e4f8f0", "subnet-2b441d71"]
+    security_group_ids = ["sg-001484f2556e719ec", "sg-fb8b0daa"]
+  }
+
+  environment {
+    variables = {
+      ASPNETCORE_ENVIRONMENT = "Production"
+      DatabaseConnectionString = var.DatabaseConnectionString
+      SECRET = var.SECRET
+      SNS_TOPIC_ARN = "arn:aws:sns:eu-west-1:714664911966:DocumentService.fifo"
+    }
+  }
+}
+
+resource "aws_lambda_permission" "apigw" {
+  statement_id  = "AllowAPIGatewayInvoke"
+  action        = "lambda:InvokeFunction"
+  function_name = "${aws_lambda_function.DocumentService.function_name}"
+  principal     = "apigateway.amazonaws.com"
+
+  # The /*/* portion grants access from any method on any resource
+  # within the API Gateway "REST API".
+  source_arn = "${aws_api_gateway_rest_api.apiGateway.execution_arn}/*/*/*"
+}
