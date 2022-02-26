1c10c38265fb907efebc8d2499c7dd41a4a7ded1
Callum Macpherson <88662046+LBHCallumM@users.noreply.github.com>
2022-02-26 17:46:39 +0000

    Implement terraform (#23)
    * Update DocumentService SnsTopic environment variable
    
    * Update Listener deploy script
    
    * Move DocumentService to seperate terraform file
    
    * Setup listener and sqs trigger
    
    * Update SQS queue visibility timeout
    
    * changesss
    
    * Restore CircleCi workflow
    
    Co-authored-by: Callum Macpherson <callummac@protonmail.com>

--- terraform/sns.tf
+++ terraform/sns.tf
@@ -13,6 +13,7 @@ resource "aws_sqs_queue" "DocumentService" {
   max_message_size          = 262144 
   message_retention_seconds = 86400 # 1 day
   receive_wait_time_seconds = 0
+  visibility_timeout_seconds = 300
  
   redrive_policy = jsonencode({
     deadLetterTargetArn = aws_sqs_queue.DocumentServiceDLQ.arn
@@ -29,6 +30,34 @@ resource "aws_sqs_queue" "DocumentServiceDLQ" {
   message_retention_seconds = 1209600 # 14 days
   receive_wait_time_seconds = 0
 }
+
+resource "aws_sqs_queue_policy" "test" {
+  queue_url = aws_sqs_queue.DocumentService.id
+
+  policy = <<POLICY
+{
+  "Version": "2008-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "sns.amazonaws.com"
+      },
+      "Action": "sqs:SendMessage",
+      "Resource": "${aws_sqs_queue.DocumentService.arn}",
+      "Condition": {
+        "ArnEquals": {
+          "aws:SourceArn": "${aws_sns_topic.DocumentService.arn}"
+        }
+      }
+    }
+  ]
+}
+POLICY
+}
+
+#
+
 
 resource "aws_sns_topic_subscription" "DocumentServiceSubsciption" {
   topic_arn = aws_sns_topic.DocumentService.arn
