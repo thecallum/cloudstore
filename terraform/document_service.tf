b85c15cecff39093dd943d864f1d71dbd6c3f4be
Callum Macpherson <callummac@protonmail.com>
2022-02-26 10:18:50 +0000

    Move DocumentService to seperate terraform file

---
+++ terraform/document_service.tf
@@ -0,0 +1,40 @@
+
+resource "aws_lambda_function" "DocumentService" {
+  function_name = "DocumentService"
+  role = "arn:aws:iam::714664911966:role/DocumentServiceRole"
+  handler = "DocumentService::DocumentService.LambdaEntryPoint::FunctionHandlerAsync"
+  runtime = "dotnetcore3.1"
+
+  s3_bucket = "terraform-state-cloudstore"
+  s3_key = "DocumentService/DocumentService.zip"
+
+  description = "description..."
+
+  timeout = 300
+  memory_size = 256
+
+  vpc_config {
+    subnet_ids         = ["subnet-a4ce21ef", "subnet-96e4f8f0", "subnet-2b441d71"]
+    security_group_ids = ["sg-001484f2556e719ec", "sg-fb8b0daa"]
+  }
+
+  environment {
+    variables = {
+      ASPNETCORE_ENVIRONMENT = "Production"
+      DatabaseConnectionString = var.DatabaseConnectionString
+      SECRET = var.SECRET
+      SNS_TOPIC_ARN = aws_sns_topic.DocumentService.arn
+    }
+  }
+}
+
+resource "aws_lambda_permission" "apigw" {
+  statement_id  = "AllowAPIGatewayInvoke"
+  action        = "lambda:InvokeFunction"
+  function_name = "${aws_lambda_function.DocumentService.function_name}"
+  principal     = "apigateway.amazonaws.com"
+
+  # The /*/* portion grants access from any method on any resource
+  # within the API Gateway "REST API".
+  source_arn = "${aws_api_gateway_rest_api.apiGateway.execution_arn}/*/*/*"
+}
