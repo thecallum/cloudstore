2c190520177d8d2472ce9ef2a9fe85a94384768c
Callum Macpherson <callummac@protonmail.com>
2022-03-03 16:13:44 +0000

    g

---
+++ terraform/document_storage.tf
@@ -0,0 +1,42 @@
+resource "aws_s3_bucket" "document_storage" {
+  bucket = "CloudStoreDocumentStorage"
+  acl    = "public-read"
+
+  tags = {
+    Name        = "CloudStoreDocumentStorage"
+    Environment = "Prod"
+  }
+
+  lifecycle_rule {
+    id      = "uploadDelete"
+    enabled = true
+
+    prefix = "upload/"
+
+    expiration {
+      days = 1
+    }
+  }
+}
+
+resource "aws_s3_bucket_policy" "allow_thumbnail_get" {
+  bucket = aws_s3_bucket.document_storage.id
+  policy = data.aws_iam_policy_document.allow_thumbnail_get.json
+}
+
+data "aws_iam_policy_document" "allow_thumbnail_get" {
+  statement {
+    principals {
+      type        = "AWS"
+      identifiers = ["*"]
+    }
+
+    actions = [
+      "s3:GetObject",
+    ]
+
+    resources = [
+      "${ aws_s3_bucket.document_storage.arn}/thumbnails/*",
+    ]
+  }
+}
