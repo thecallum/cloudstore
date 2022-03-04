78a4f9996031180e3d29930337f2345a1a46a15b
Callum Macpherson <callummac@protonmail.com>
2022-03-04 14:07:52 +0000

    missing file

---
+++ terraform/sns_roles.tf
@@ -0,0 +1,45 @@
+resource "aws_iam_role" "sns_feedback_role" {
+  name = "sns_feedback_role"
+
+  assume_role_policy = jsonencode({
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Effect": "Allow",
+            "Action": [
+                "logs:CreateLogGroup",
+                "logs:CreateLogStream",
+                "logs:PutLogEvents",
+                "logs:PutMetricFilter",
+                "logs:PutRetentionPolicy"
+            ],
+            "Resource": [
+                "*"
+            ]
+        }
+    ]
+  })
+}
+
+# resource "aws_iam_role" "sns_success_feedback_role" {
+#   name = "sns_success_feedback_role"
+
+#   assume_role_policy = jsonencode({
+#     "Version": "2012-10-17",
+#     "Statement": [
+#         {
+#         "Effect": "Allow",
+#         "Action": [
+#             "logs:CreateLogGroup",
+#             "logs:CreateLogStream",
+#             "logs:PutLogEvents",
+#             "logs:PutMetricFilter",
+#             "logs:PutRetentionPolicy"
+#         ],
+#         "Resource": [
+#             "*"
+#         ]
+#         }
+#     ]
+#   })
+# }
