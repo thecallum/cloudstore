9757d496aa8af9f0bb10f6f632cb54f4b48e0dc1
Callum Macpherson <callummac@protonmail.com>
2022-03-04 13:39:32 +0000

    updates

--- terraform/document_service_roles.tf
+++ terraform/document_service_roles.tf
@@ -1,7 +1,3 @@
-data "aws_iam_policy" "AWSLambdaBasicExecutionRole" {
-  arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
-}
-
 resource "aws_iam_role" "document_service_role" {
   name = "document_service_role"
 
@@ -43,7 +39,12 @@ resource "aws_iam_policy" "document_service_s3_access" {
                 "s3:PutObjectTagging",
                 "s3:DeleteObject"
             ],
-            "Resource": "${aws_s3_bucket.document_storage.arn}/*"
+            "Resource": "${aws_s3_bucket.document_storage.arn}/*",
+             "Principal": {
+                "AWS": [
+                    aws_lambda_function.DocumentService.arn
+                ]
+            }
         }
     ]
   })
@@ -80,30 +81,3 @@ resource "aws_iam_role_policy_attachment" "a" {
    role       = "${aws_iam_role.document_service_role.name}"
    policy_arn = "${data.aws_iam_policy.AWSLambdaBasicExecutionRole.arn}"
 }
-
-resource "aws_iam_role" "document_service_listener_role" {
-  name = "document_service_listener_role"
-
-  assume_role_policy = jsonencode({
-    "Version": "2012-10-17",
-    "Statement": [
-        {
-            "Effect": "Allow",
-            "Action": [
-                "sts:AssumeRole"
-            ],
-            "Principal": {
-                "Service": [
-                    "lambda.amazonaws.com"
-                ]
-            }
-        }
-    ]
-  })
-}
-
-resource "aws_iam_role_policy_attachment" "b" {
-   role       = "${aws_iam_role.document_service_listener_role.name}"
-   policy_arn = "${data.aws_iam_policy.AWSLambdaBasicExecutionRole.arn}"
-}
-
