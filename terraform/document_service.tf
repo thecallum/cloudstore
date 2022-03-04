58b999e3e17e599fdf0a2e39ecb3143a743ae239
Callum Macpherson <callummac@protonmail.com>
2022-03-04 11:49:51 +0000

    ddd

--- terraform/document_service.tf
+++ terraform/document_service.tf
@@ -34,26 +34,24 @@ resource "aws_lambda_function" "DocumentService" {
 resource "aws_iam_role" "document_service_role" {
   name = "document_service_role"
 
-  # Terraform's "jsonencode" function converts a
-  # Terraform expression result to valid JSON syntax.
-  # assume_role_policy = jsonencode({
-  #   Version = "2012-10-17"
-  #   Statement = [
-  #     {
-  #       "Effect": "Allow",
-  #       "Action": [
-  #           "logs:CreateLogGroup",
-  #           "logs:CreateLogStream",
-  #           "logs:PutLogEvents"
-  #       ],
-  #       "Resource": "*"
-  #     }
-  #   ]
-  # })
-
-  tags = {
-    tag-key = "tag-value"
-  }
+  assume_role_policy = <<EOF
+    {
+        "Version": "2012-10-17",
+        "Statement": [
+            {
+                "Effect": "Allow",
+                "Action": [
+                    "sts:AssumeRole"
+                ],
+                "Principal": {
+                    "Service": [
+                        "lambda.amazonaws.com"
+                    ]
+                }
+            }
+        ]
+    }
+  EOF
 }
 
 data "aws_iam_policy" "AWSLambdaBasicExecutionRole" {
