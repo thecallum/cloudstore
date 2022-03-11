93ef5afb7ec655fca1158b425622337f752a38c9
Callum Macpherson <callummac@protonmail.com>
2022-03-11 09:27:08 +0000

    Add EntityHelper

---
+++ backend/DocumentServiceListener/DocumentServiceListener/DocumentServiceListener/Helpers/EntityHelper.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DocumentServiceListener.Helpers
+{
+    public static class EntityHelper
+    {
+        public static Guid? TryParseGuid(Dictionary<string, object> body, string name)
+        {
+            if (!body.ContainsKey(name)) return null;
+
+            var paramter = body[name].ToString();
+
+            try
+            {
+                return Guid.Parse(paramter);
+            }
+            catch (Exception)
+            {
+                return null;
+            }
+        }
+    }
+}
