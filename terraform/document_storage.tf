87b7a923481189aacca13525eb60b8e5387caa92
Callum Macpherson <88662046+LBHCallumM@users.noreply.github.com>
2022-04-01 12:25:34 +0100

    Update S3Terraform (#37)
    Co-authored-by: Callum Macpherson <callummac@protonmail.com>

--- terraform/document_storage.tf
+++ terraform/document_storage.tf
@@ -1,29 +1,9 @@
 resource "aws_s3_bucket" "document_storage" {
   bucket = "cloudstore-document-storage"
-  acl    = "public-read"
 
   tags = {
     Name        = "CloudStoreDocumentStorage"
     Environment = "Prod"
-  }
-
-  lifecycle_rule {
-    id      = "uploadDelete"
-    enabled = true
-
-    prefix = "upload/"
-
-    expiration {
-      days = 1
-    }
-  }
-
-  cors_rule {
-    allowed_headers = ["*"]
-    allowed_methods = ["PUT", "POST", "GET"]
-    allowed_origins = ["*"]
-    expose_headers  = []
-    max_age_seconds = 3000
   }
 }
 
@@ -46,5 +26,39 @@ data "aws_iam_policy_document" "allow_thumbnail_get" {
     resources = [
       "${aws_s3_bucket.document_storage.arn}/thumbnails/*",
     ]
+  }
+}
+
+resource "aws_s3_bucket_acl" "document_storage_acl" {
+  bucket = aws_s3_bucket.document_storage.id
+  acl    = "public-read"
+}
+
+resource "aws_s3_bucket_cors_configuration" "document_storage_cors" {
+  bucket = aws_s3_bucket.document_storage.id
+
+  cors_rule {
+    allowed_headers = ["*"]
+    allowed_methods = ["PUT", "POST", "GET"]
+    allowed_origins = ["*"]
+    expose_headers  = []
+    max_age_seconds = 3000
+  }
+}
+
+resource "aws_s3_bucket_lifecycle_configuration" "document_storage_lifecycle" {
+  bucket = aws_s3_bucket.document_storage.id
+
+  rule {
+    id      = "uploadDelete"
+    status = "Enabled"
+
+    filter {
+      prefix = "upload/"
+    }
+
+    expiration {
+      days = 1
+    }
   }
 }
