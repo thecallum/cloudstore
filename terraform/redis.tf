cc8b372cd103d17a51b4167f8937f181b2b00696
Callum Macpherson <88662046+LBHCallumM@users.noreply.github.com>
2022-04-01 12:05:04 +0100

    Update Terraform Redis NodeType (#36)
    * Fix redis nodeType
    
    Co-authored-by: Callum Macpherson <callummac@protonmail.com>

--- terraform/redis.tf
+++ terraform/redis.tf
@@ -1,7 +1,7 @@
 resource "aws_elasticache_cluster" "redis" {
   cluster_id           = "redis-cache"
   engine               = "redis"
-  node_type            = "cache.m2.micro"
+  node_type            = "cache.t2.micro"
   num_cache_nodes      = 1
   parameter_group_name = "default.redis3.2"
   engine_version       = "3.2.10"
