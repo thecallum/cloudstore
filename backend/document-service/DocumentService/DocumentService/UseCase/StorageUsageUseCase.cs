9c83c31c81e7e611eae4c3a6f9c3199da40e25ea
Callum Macpherson <callummac@protonmail.com>
2022-04-03 09:48:08 +0100

    Add logging to StorageUsageUseCase

--- backend/document-service/DocumentService/DocumentService/UseCase/StorageUsageUseCase.cs
+++ backend/document-service/DocumentService/DocumentService/UseCase/StorageUsageUseCase.cs
@@ -7,6 +7,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using DocumentService.Logging;
 
 namespace DocumentService.UseCase
 {
@@ -23,25 +24,37 @@ namespace DocumentService.UseCase
 
         public async Task<long> GetUsage(User user)
         {
+            LogHelper.LogUseCase("GetUsage");
+
             var cachedValue = await _cache.GetValue(user.Id);
+            Console.WriteLine($"Reading StorageUsage cachedValue: [{cachedValue}] [{user.Id}]");
+
             if (cachedValue != null) return (long) cachedValue;
 
             // get value from database
             var storageUsageResponse = await _documentGateway.GetUsage(user);
+            Console.WriteLine($"Reading StorageUsage from databse");
 
             await _cache.UpdateValue(user.Id, storageUsageResponse.StorageUsage);
+            Console.WriteLine($"Saving database response to cache: [{storageUsageResponse.StorageUsage}]");
 
             return storageUsageResponse.StorageUsage;
         }
 
         public async Task UpdateUsage(User user, long difference)
         {
+            LogHelper.LogUseCase("UpdateUsage");
+
             var cachedValue = await _cache.GetValue(user.Id);
+
+            Console.WriteLine($"Reading StorageUsage cachedValue: [{cachedValue}] [{user.Id}]");
 
             long newValue;
 
             if (cachedValue == null)
             {
+                Console.WriteLine($"Reading storageUsage from database");
+
                 // get value from database
                 var storageUsageResponse = await _documentGateway.GetUsage(user);
 
@@ -49,8 +62,12 @@ namespace DocumentService.UseCase
             } else
             {
                 newValue = (long) cachedValue + difference;
+
+                Console.WriteLine($"Updating using cachedValue: [{newValue}]");
             }
             
+            Console.WriteLine($"Updating storageUsage: [{newValue}]");
+
             await _cache.UpdateValue(user.Id, newValue);
         }
     }
