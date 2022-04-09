64505046b2308d21df43514664c9e3389e694e01
Callum Macpherson <callummac@protonmail.com>
2022-04-09 11:23:23 +0100

    test

--- terraform/redis.tf
+++ terraform/redis.tf
@@ -1,11 +1,13 @@
 resource "aws_elasticache_cluster" "redis" {
-  cluster_id           = "redis-cache"
-  engine               = "redis"
-  node_type            = "cache.t2.micro"
-  num_cache_nodes      = 1
-  parameter_group_name = "default.redis3.2"
-  engine_version       = "3.2.10"
-  port                 = 6379
-  security_group_ids   = [aws_security_group.redis_security_group.id]
-  description          = "Redis Cache for CloudStore"
+  cluster_id             = "redis-cache"
+  engine                 = "redis"
+  node_type              = "cache.t2.micro"
+  num_cache_nodes        = 1
+  parameter_group_name   = "default.redis3.2"
+  engine_version         = "3.2.10"
+  port                   = 6379
+  description            = "Redis Cache for CloudStore"
+  vpc_security_group_ids = [
+    aws_security_group.redis_security_group.id
+  ]
 }
