576e6cf09b455b7bb82104f5b39a34be0c491bba
Callum Macpherson <callummac@protonmail.com>
2022-04-09 11:19:17 +0100

    Update Redis security group

---
+++ redis_roles.tf
@@ -0,0 +1,21 @@
+resource "aws_security_group" "redis_security_group" {
+  name        = "redis_security_group"
+  description = "RedisCache security Group"
+
+  ingress {
+    description      = "Allow access from DocumentService"
+    protocol         = "tcp"
+    from_port        = "6379"
+    to_port          = "6379"
+    # cidr_blocks      = ["0.0.0.0/0"]
+    source_security_group_id = aws_security_group.document_service_security_group.id
+  }
+
+  egress {
+    description      = "Allow output to any service"
+    protocol         = "tcp"
+    from_port        = "6379"
+    to_port          = "6379"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
--- terraform/redis.tf
+++ terraform/redis.tf
@@ -6,26 +6,6 @@ resource "aws_elasticache_cluster" "redis" {
   parameter_group_name = "default.redis3.2"
   engine_version       = "3.2.10"
   port                 = 6379
-  security_group_ids   = []
-}
-
-resource "aws_security_group" "redis_security_group" {
-  name        = "redis_security_group"
-  description = "RedisCache security Group"
-
-  ingress {
-    description      = "Temp Allow All"
-    protocol         = "tcp"
-    from_port        = "0"
-    to_port          = "6553"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    description      = "Temp Allow All"
-    protocol         = "tcp"
-    from_port        = "0"
-    to_port          = "6553"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+  security_group_ids   = [aws_security_group.redis_security_group.id]
+  description          = "Redis Cache for CloudStore"
 }
